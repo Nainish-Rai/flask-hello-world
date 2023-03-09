7cda37ab6eeeb68a544ca252c05db9da79989f1c
Nainish Rai <109546113+Nainish-Rai@users.noreply.github.com>
2023-03-09 14:56:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from ytmusicapi import YTMusic
+
+ytmusic = YTMusic()
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route("/search/<query>")
+def search(query):
+    return   ytmusic.search(query)
